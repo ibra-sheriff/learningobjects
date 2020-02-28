23106e2981ba2d5f2fc8339ed4d42eb7bc28a7d1
Ibrahim Sheriff Sururu <21066442@sun.ac.za>
2020-02-28 18:20:35 +0200

    quick BoringSun fix

--- BoringSun.java
+++ BoringSun.java
@@ -1,7 +1,7 @@
 public class BoringSun {
 	public static void main(String[] ibra) {
 		while (true) {
-			System.out.print(">);
+			System.out.print(">");
 			String line = StdIn.readLine();
 			String[] inputs = line.split("\\s+");
 		}
